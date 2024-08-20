c2cef47c144aa22f54193480c4ce28d23992d0e9
Francois Lancelot <16709438+francoislanc@users.noreply.github.com>
2024-08-21 00:28:32 +0200

    Add audio analysis functions
    Based on MidiCaps repo

---
+++ backend/src/midistral/audio_analysis.py
@@ -0,0 +1,170 @@
+import json
+from collections import Counter
+from functools import lru_cache
+from pathlib import Path
+
+import essentia
+import numpy as np
+from chord_extractor.extractors import Chordino
+from essentia.standard import (
+    MonoLoader,
+    TensorflowPredict2D,
+    TensorflowPredictEffnetDiscogs,
+)
+
+essentia.log.warningActive = False
+essentia.log.infoActive = False
+
+MODEL_FOLDER = Path(__file__).resolve().parent.parent.parent / "models"
+
+
+@lru_cache
+def get_audio_embedding_model():
+    model_path = MODEL_FOLDER / "discogs-effnet-bs64-1.pb"
+
+    embedding_model = TensorflowPredictEffnetDiscogs(
+        graphFilename=str(model_path), output="PartitionedCall:1"
+    )
+
+    return embedding_model
+
+
+@lru_cache
+def get_genre_model():
+    model_path = MODEL_FOLDER / "mtg_jamendo_genre-discogs-effnet-1.pb"
+    metadata_path = MODEL_FOLDER / "mtg_jamendo_genre-discogs-effnet-1.json"
+    model = TensorflowPredict2D(graphFilename=str(model_path))
+    metadata = str(metadata_path)
+    return model, metadata
+
+
+@lru_cache
+def get_mood_model():
+    model_path = MODEL_FOLDER / "mtg_jamendo_moodtheme-discogs-effnet-1.pb"
+    metadata_path = MODEL_FOLDER / "mtg_jamendo_moodtheme-discogs-effnet-1.json"
+    model = TensorflowPredict2D(graphFilename=str(model_path))
+    metadata = str(metadata_path)
+    return model, metadata
+
+
+@lru_cache
+def get_chord_estimator():
+    return Chordino()
+
+
+def get_mtg_tags(embeddings, tag_model, tag_json, max_num_tags=5, tag_threshold=0.01):
+
+    with open(tag_json, "r") as json_file:
+        metadata = json.load(json_file)
+    predictions = tag_model(embeddings)
+    mean_act = np.mean(predictions, 0)
+    ind = np.argpartition(mean_act, -max_num_tags)[-max_num_tags:]
+    tags = []
+    confidence_score = []
+    for i in ind:
+        # print(metadata['classes'][i] + str(mean_act[i]))
+        if mean_act[i] > tag_threshold:
+            tags.append(metadata["classes"][i])
+            confidence_score.append(mean_act[i])
+    ind = np.argsort(-np.array(confidence_score))
+    tags = [tags[i] for i in ind]
+    confidence_score = np.round((np.array(confidence_score)[ind]).tolist(), 4).tolist()
+
+    return tags, confidence_score
+
+
+def get_chords(wav_audio_path: Path):
+    chord_estimator = get_chord_estimator()
+    chords = chord_estimator.extract(str(wav_audio_path))
+    chords_out = [(x.chord, x.timestamp) for x in chords[1:-1]]
+
+    # chord summary
+    ch_name = []
+    ch_time = []
+    for ch in chords_out:
+        ch_name.append(ch[0])
+        ch_time.append(ch[1])
+    if len(ch_name) < 3:
+        final_seq = ch_name
+        final_count = 1
+    else:
+        final_seq, final_count = _give_me_final_seq(ch_name)
+    if final_seq is not None:
+        if len(final_seq) == 4:
+            if final_seq[0] == final_seq[2] and final_seq[1] == final_seq[3]:
+                final_seq = final_seq[0:2]
+    chord_summary = [final_seq, final_count]
+    return chords_out, chord_summary[0]
+
+
+def get_mood_and_genre(wav_audio_path: Path):
+    audio = MonoLoader(
+        filename=str(wav_audio_path),
+        sampleRate=16000,
+        resampleQuality=1,
+    )()
+
+    embedding_model = get_audio_embedding_model()
+    genmodel, genre_metadata = get_genre_model()
+    moodmodel, mood_metadata = get_mood_model()
+
+    embeddings = embedding_model(audio)
+    mood_tags, mood_cs = get_mtg_tags(
+        embeddings,
+        moodmodel,
+        mood_metadata,
+        max_num_tags=5,
+        tag_threshold=0.02,
+    )
+    genre_tags, genre_cs = get_mtg_tags(
+        embeddings,
+        genmodel,
+        genre_metadata,
+        max_num_tags=4,
+        tag_threshold=0.05,
+    )
+
+    return mood_tags, mood_cs, genre_tags, genre_cs
+
+
+def _find_most_repeating_sequence(chords_list, sequence_length):
+    sequences = [
+        tuple(chords_list[i : i + sequence_length])
+        for i in range(len(chords_list) - sequence_length + 1)
+    ]
+    delete_index = []
+    for i, seqs in enumerate(sequences):
+        if seqs[0] == seqs[-1]:
+            delete_index.append(i)
+    for i in reversed(delete_index):
+        sequences.pop(i)
+    sequence_counts = Counter(sequences)
+    try:
+        most_common_sequence, count = sequence_counts.most_common(1)[0]
+        return most_common_sequence, count
+    except Exception:
+        # print(e)
+        return None, 0
+
+
+def _give_me_final_seq(chords):
+    sequence_3, count_3 = _find_most_repeating_sequence(chords, 3)
+    sequence_4, count_4 = _find_most_repeating_sequence(chords, 4)
+    sequence_5, count_5 = _find_most_repeating_sequence(chords, 5)
+    total_count = count_3 + count_4 + count_5
+    if count_5 > 0.25 * (total_count):
+        if count_5 > 0.79 * count_4:
+            return sequence_5, count_5
+    if count_4 > 0.3 * (total_count):
+        if count_4 > 0.79 * count_3:
+            return sequence_4, count_4
+    if count_3 == 0:
+        if count_4 == 0:
+            if count_5 == 0:
+                return None, 0  # everything is 0
+            else:
+                return sequence_5, count_5
+        else:
+            return sequence_4, count_4
+    else:
+        return sequence_3, count_3
--- backend/src/midistral/midi_utils.py
+++ backend/src/midistral/midi_utils.py
@@ -1,10 +1,13 @@
 import logging
 import subprocess
 import tempfile
+from collections import defaultdict
 from pathlib import Path
 from typing import Optional
 
 import mido
+import numpy as np
+from music21 import midi as music21midi
 
 from midistral.abc_utils import clean_generated_abc
 
@@ -17,11 +20,7 @@ def convert_midi_to_ogg(midi_path: Path, ogg_path: Path):
         "-o",
         str(ogg_path.resolve()),
     ]
-    p = subprocess.run(cmd, capture_output=True, text=True)
-    if len(p.stdout) > 0:
-        logging.debug(f"convert_midi_to_ogg {midi_path} - {p.stdout}")
-    if len(p.stderr) > 0:
-        logging.debug(f"convert_midi_to_ogg {midi_path} - {p.stderr}")
+    subprocess.run(cmd, capture_output=True)
 
 
 def convert_midi_to_abc(midi_path: Path, abc_path: Path):
@@ -97,9 +96,9 @@ def get_midi_and_ogg_from_abc(abc_notation: str) -> bytes:
 
 
 def get_midi_tracks_nums(midi_path: Path) -> int:
-    mid = mido.MidiFile(midi_path)
+    midi = mido.MidiFile(midi_path)
     num = 0
-    for t in mid.tracks:
+    for t in midi.tracks:
         for m in t:
             if (
                 isinstance(m, mido.messages.messages.Message)
@@ -107,3 +106,283 @@ def get_midi_tracks_nums(midi_path: Path) -> int:
             ):
                 num += 1
     return num
+
+
+def get_duration(midi_path: Path) -> int:
+    try:
+        midi = mido.MidiFile(midi_path)
+        return midi.length
+    except Exception:
+        return -1
+
+
+def get_tempo(midi_path: Path) -> Optional[int]:
+    midi = mido.MidiFile(midi_path)
+    try:
+        for msg in midi:
+            if msg.type == "set_tempo":
+                tempo = mido.tempo2bpm(msg.tempo)
+                return tempo
+    except Exception:
+        return None
+    return None
+
+
+def read_midi(midi_path: Path):
+    mf = music21midi.MidiFile()
+    mf.open(midi_path)
+    mf.read()
+    mf.close()
+    return music21midi.translate.midiFileToStream(mf)
+
+
+def get_key(midi_path: Path) -> Optional[str]:
+    midi = read_midi(midi_path)
+    # key
+    try:
+        res_key = midi.analyze("keys")
+        key = res_key.tonic.name + " " + res_key.mode
+    except Exception:
+        key = None
+    # key postprocessing
+    if "-" in key:
+        key = key.replace("-", "b")
+
+    return key
+
+
+def get_time_signature(midi_path: Path) -> Optional[str]:
+    midi = read_midi(midi_path)
+
+    try:
+        time_sig = midi.getTimeSignatures()[0]
+        time_signature = str(time_sig.numerator) + "/" + str(time_sig.denominator)
+    except Exception:
+        time_signature = None
+
+    return time_signature
+
+
+def get_instruments(midi_file_path: Path):
+    # Dictionary to store instrument durations
+    instrument_durations = defaultdict(float)
+    instrument_names = []
+    instrument_channels = []
+    instrument_change_times = []
+    # Parse MIDI file
+    midi = mido.MidiFile(midi_file_path)
+    # Iterate through each track
+    for track in midi.tracks:
+        # Dictionary to store note-on events for each instrument
+        active_notes = defaultdict(float)
+        last_event_time = 0
+        # Iterate through each event in the track
+        for msg in track:
+            # Update the time since the last event
+            delta_time = msg.time
+            last_event_time += delta_time
+            if msg.type == "program_change":
+                prog = msg.program
+                chan = msg.channel
+                # if chan==9 and prog==0:
+                if chan == 9 and not 111 < prog < 120:
+                    prog = 128
+                if chan in instrument_channels:
+                    instrument_names[instrument_channels.index(chan)] = (
+                        prog  # replace the existing instrument in this channel, no ambiguity!!!
+                    )
+                else:
+                    instrument_names.append(prog)
+                    instrument_channels.append(chan)
+                instrument_change_times.append(msg.time)
+                # print(msg.time)
+            # If it's a note-on or note-off event
+            if msg.type == "note_on" or msg.type == "note_off":
+                # Extract the instrument (channel) and note number
+                channel = msg.channel
+                note = msg.note
+                # Calculate the duration since the last event
+                duration = last_event_time - active_notes[(channel, note)]
+                active_notes[(channel, note)] = last_event_time
+                # Accumulate the duration for this instrument
+                instrument_durations[channel] += duration
+    new_dict = sorted(instrument_durations.items(), key=lambda x: x[1], reverse=True)
+    if len(instrument_names) > 20:
+        print("too many instruments in this one!")
+        print(midi_file_path)
+        return [], []
+    sorted_instrument_list = []
+    how_many = min(5, len(set(instrument_names)))
+    if how_many == 0:
+        return []
+    add_drums = False
+    if 9 not in instrument_channels:
+        for rr in new_dict:
+            if 9 in rr:
+                add_drums = True
+                break
+            else:
+                add_drums = False
+    if add_drums:
+        instrument_names.append(128)
+        instrument_channels.append(9)
+    for inst in new_dict:
+        try:
+            sorted_instrument_list.append(
+                instrument_names[instrument_channels.index(inst[0])]
+            )
+        except Exception as e:
+            print(e)
+            print(midi_file_path)
+            return sorted_instrument_list
+
+    out_inst_list = []
+    for inst in sorted_instrument_list:
+        out_inst_list.append(get_instrument_name(inst))
+    # instruments summary - only add one instance of each instrument, then keep top 5
+    out_inst_sum_list = []
+    for rr in out_inst_list:
+        if rr not in out_inst_sum_list:
+            out_inst_sum_list.append(rr)
+    how_many = np.min((5, len(out_inst_sum_list)))
+    instruments_summary = out_inst_sum_list[0:how_many]
+
+    return sorted_instrument_list, instruments_summary
+
+
+def get_instrument_name(number: int) -> Optional[str]:
+    instruments_mapping = {
+        0: "Piano",
+        1: "Piano",
+        2: "Electric Piano",
+        3: "Honky-tonk Piano",
+        4: "Piano",
+        5: "Piano",
+        6: "Harpsichord",
+        7: "Clavinet",
+        8: "Celesta",
+        9: "Glockenspiel",
+        10: "Music box",
+        11: "Vibraphone",
+        12: "Marimba",
+        13: "Xylophone",
+        14: "Tubular Bells",
+        15: "Dulcimer",
+        16: "Hammond Organ",
+        17: "Percussive Organ",
+        18: "Rock Organ",
+        19: "Church Organ",
+        20: "Reed Organ",
+        21: "Accordion",
+        22: "Harmonica",
+        23: "Tango Accordion",
+        24: "Acoustic Guitar",
+        25: "Acoustic Guitar",
+        26: "Electric Guitar",
+        27: "Clean Electric Guitar",
+        28: "Electric Guitar",
+        29: "Overdriven Guitar",
+        30: "Distortion Guitar",
+        31: "Guitar Harmonics",
+        32: "Acoustic Bass",
+        33: "Electric Bass",
+        34: "Electric Bass",
+        35: "Fretless Bass",
+        36: "Slap Bass",
+        37: "Slap Bass",
+        38: "Synth Bass",
+        39: "Synth Bass",
+        40: "Violin",
+        41: "Viola",
+        42: "Cello",
+        43: "Contrabass",
+        44: "Tremolo Strings",
+        45: "Pizzicato Strings",
+        46: "Orchestral Harp",
+        47: "Timpani",
+        48: "String Ensemble",
+        49: "String Ensemble",
+        50: "Synth Strings",
+        51: "Synth Strings",
+        52: "Choir Aahs",
+        53: "Voice Oohs",
+        54: "Synth Voice",
+        55: "Orchestra Hit",
+        56: "Trumpet",
+        57: "Trombone",
+        58: "Tuba",
+        59: "Muted Trumpet",
+        60: "French Horn",
+        61: "Brass Section",
+        62: "Synth Brass",
+        63: "Synth Brass",
+        64: "Soprano Saxophone",
+        65: "Alto Saxophone",
+        66: "Tenor Saxophone",
+        67: "Baritone Saxophone",
+        68: "Oboe",
+        69: "English Horn",
+        70: "Bassoon",
+        71: "Clarinet",
+        72: "Piccolo",
+        73: "Flute",
+        74: "Recorder",
+        75: "Pan Flute",
+        76: "Bottle Blow",
+        77: "Shakuhachi",
+        78: "Whistle",
+        79: "Ocarina",
+        80: "Synth Lead",
+        81: "Synth Lead",
+        82: "Calliope Lead",
+        83: "Chiffer Lead",
+        84: "Charang Lead",
+        85: "Synth Voice",
+        86: "Synth Lead",
+        87: "Brass Lead",
+        88: "Synth Pad",
+        89: "Synth Pad",
+        90: "Synth Pad",
+        91: "Synth Pad",
+        92: "Synth Pad",
+        93: "Synth Pad",
+        94: "Synth Pad",
+        95: "Synth Pad",
+        96: "Synth Effects",
+        97: "Synth Effects",
+        98: "Synth Effects",
+        99: "Synth Effects",
+        100: "Synth Effects",
+        101: "Synth Effects",
+        102: "Synth Effects",
+        103: "Synth Effects",
+        104: "Sitar",
+        105: "Banjo",
+        106: "Shamisen",
+        107: "Koto",
+        108: "Kalimba",
+        109: "Bagpipe",
+        110: "Fiddle",
+        111: "Shana",
+        112: "Tinkle Bell",
+        113: "Agogo",
+        114: "Steel Drums",
+        115: "Woodblock",
+        116: "Taiko Drum",
+        117: "Melodic Tom",
+        118: "Synth Drum",
+        119: "Reverse Cymbal",
+        120: "Guitar Fret Noise",
+        121: "Breath Noise",
+        122: "Seashore",
+        123: "Bird Tweet",
+        124: "Telephone Ring",
+        125: "Helicopter",
+        126: "Applause",
+        127: "Gunshot",
+        128: "Drums",
+    }
+    if number in instruments_mapping:
+        return instruments_mapping[number]
+    else:
+        return None
